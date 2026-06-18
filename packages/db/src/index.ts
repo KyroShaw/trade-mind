618db52e6f1c7a22db5b524cdd70636249419ab8
KyroShaw <choola944@gmail.com>
2026-06-19 01:08:32 +0800

    init all

---
+++ packages/db/src/index.ts
@@ -0,0 +1,10 @@
+import { env } from "@trade-mind/env/server";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+import * as schema from "./schema";
+
+export function createDb() {
+  return drizzle(env.DATABASE_URL, { schema });
+}
+
+export const db = createDb();
